f2185d80e1182b8b4a91c6ebdf6fa7feb69abef0
Jonathan Donaldson <jon@than.dev>
2023-07-06 23:13:55 +0100

    Fix doc

--- eventastic/src/event.rs
+++ eventastic/src/event.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::Version;
 
-/// An [`Event`] that has been will be / has been persisted to the Event Store.
+/// An [`Event`] that will be / has been persisted to the Event Store.
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct EventStoreEvent<Id, Evt>
 where
